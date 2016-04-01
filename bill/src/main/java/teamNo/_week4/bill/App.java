cf26032adbe51a0fa98c81d907675f34733b3601
jo930_000 <jo930_000@sec>
2016-04-01 19:05:56 +0900

    a

--- bill/src/main/java/teamNo/_week4/bill/App.java
+++ bill/src/main/java/teamNo/_week4/bill/App.java
@@ -7,6 +7,7 @@ public class App
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
+        Cost cost = new Cost();
         
         System.out.print("사용자의 통화정보를 입력하세요 : [Gold or Silver], [Addline], [Total Traffic]");
         String plan = scan.nextLine();
@@ -15,12 +16,12 @@ public class App
         
         if(plan.toUpperCase()=="GOLD")
         {
-        	GoldPlan gold = new GoldPlan();
+        	cost.plan = new GoldPlan();
         }
         
         else if(plan.toUpperCase()=="SILVER")
         {
-        	SilverPlan silver = new SilverPlan();
+        	cost.plan = new SilverPlan();
         }
     }
 }
