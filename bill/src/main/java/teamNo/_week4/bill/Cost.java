30ec541b5d9805bbda2b611de1f4b9456d383a5d
sdchihi <sdchihi@naver.com>
2016-04-01 18:33:09 +0900

    ddd

--- bill/src/main/java/teamNo/_week4/bill/Cost.java
+++ bill/src/main/java/teamNo/_week4/bill/Cost.java
@@ -14,6 +14,10 @@ public class Cost {
 		return userUsed;
 	}
 	
+	public int getNumLine(int num){
+		return num;
+	}
+	
 	public double calculate_overflow_traffic_cost(){
 		double expected_bill = 0;
 		if(plan.getGrade() == GOLD){
@@ -30,7 +34,27 @@ public class Cost {
 	
 	public double calculate_total_cost(){
 		double total_cost = 0;
-		total_cost = plan.getBasecost() + this.calculate_overflow_traffic_cost();
+		total_cost = plan.getBasecost() + this.calculate_overflow_traffic_cost() + this.calculate_Addline_Cost();
 		return total_cost;
 	}
+	
+	public double calculate_Addline_Cost(){
+		get_Number_Of_Line = getNumLine(num);
+		if(plan.getGrade()==GOLD){
+			if (get_Number_Of_Line < 4)
+				return 14.50 * (get_Number_Of_Line-1); 
+			else 
+				return ( 14.50 * 2 ) + ( 5 * (get_Number_Of_Line-3) );
+			
+		}	
+		else(plan.getGrade()==SILVER){
+			if(get_number_of_Line <4)
+				return 21.50 * (get_Number_Of_Line-1);
+			else
+				return 21.50 * 2 + ( 5 * ( get_Number_Of_Line - 3 ) );
+		}
+	}
+	
+	
 }
+
