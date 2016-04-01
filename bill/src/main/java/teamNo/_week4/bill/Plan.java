1f47e9dbda0e13e987c6d35eedf8bac01d2f0d22
jo930_000 <jo930_000@sec>
2016-04-01 17:10:48 +0900

    a

---
+++ bill/src/main/java/teamNo/_week4/bill/Plan.java
@@ -0,0 +1,15 @@
+public interface Plan {
+		
+	public void setBasecost(int base_cost);
+	
+	public int getBasecost();
+	
+	public void setAddLineCost(int addline_cost);
+	
+	public int getAddLineCost();
+	
+	public void setOverflowTrafficCost(int overflow_traffic_cost);
+	
+	public int getOverflowTrafficCost();
+	
+}
