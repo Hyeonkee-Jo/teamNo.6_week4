8804cb9ee36148242f3669978f9e5fd9b266648b
jo930_000 <jo930_000@sec>
2016-04-03 16:07:50 +0900

    d

--- bill/src/main/java/teamsix/App.java
+++ bill/src/main/java/teamsix/App.java
@@ -9,6 +9,7 @@ public class App {
 	}
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger("Bill");
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		String plan = scan.nextLine();
 		int traffic = scan.nextInt();
