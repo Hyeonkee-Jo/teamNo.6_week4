aa8a75d75a3c78b60a468cf31219f31498578249
Hyeonkee-Jo <201202143@cs-cnu.org>
2016-04-10 20:49:59 +0900

    test1

--- bill/src/test/java/teamsix/AppTest.java
+++ bill/src/test/java/teamsix/AppTest.java
@@ -1,38 +1,35 @@
 package teamsix;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest{
+	  
+	private Cost c;
+	
+	@Before
+	public void setUp(){
+		c = new Cost(null, 0, 0);
+	}
+	
+	private void set(String p, int traffic , int num){
+		c.traffic = traffic;
+		c.num = num;
+		if(("S").equals(p)){
+			c.plan = new SilverPlan();		
+		}
+		else if(("G").equals(p)) {
+			c.plan = new GoldPlan();
+		}
+	}
+	
+	@Test
+	public void testGoldOnelineBelowThau() {
+		set("G", 700, 1);
+		assertEquals("49.95$", c.toString());
+	}
 }
